42cd9555ad4278b5ad1aa4a8ebafaa3eee6855a1
bblum9 <bblum@bu.edu>
2020-06-06 12:43:06 -0400

    Updates

--- Notebook.py
+++ Notebook.py
@@ -10,7 +10,7 @@ pipeline_path = os.path.normpath(notebook_path + "/src" + "/Pipeline.py")
 r_path = os.path.normpath(notebook_path + "/src" + "/Pipeline.R")
 
 # Run pipeline scripts
-p = subprocess.Popen(args=["python", pipeline_path], stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+p = subprocess.Popen(args=["python3", pipeline_path], stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
 analysis_dir = str(p.communicate()[0], 'utf-8').rstrip()
 
 subprocess.Popen(args=["Rscript", r_path, notebook_path, analysis_dir])
