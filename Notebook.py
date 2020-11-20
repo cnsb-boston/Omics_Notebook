11d20663bf35c945978097bca5abbda9f33a3631
bblum9 <bblum@bu.edu>
2020-11-20 17:49:25 -0500

    Updates

--- Notebook.py
+++ Notebook.py
@@ -11,17 +11,23 @@ notebook_path = os.path.dirname(os.path.abspath(__file__))
 pipeline_path = os.path.normpath(notebook_path + "/src" + "/Pipeline.py")
 r_path = os.path.normpath(notebook_path + "/src" + "/Pipeline.R")
 
+
 # Run pipeline scripts
 p = subprocess.Popen(args=["python3", pipeline_path], stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
 analysis_dir = str(p.communicate()[0], 'utf-8').rstrip()
 
-
 # 1. Run natively
-subprocess.Popen(args=["Rscript", r_path, notebook_path, analysis_dir])
+if len(sys.argv)<1 : 
+  print("Running natively.")
+  subprocess.Popen(args=["Rscript", r_path, notebook_path, analysis_dir])
 
 # 2. Run with Docker
-# subprocess.Popen(args=["docker run -it --rm -u docker -v ", notebook_path,":/home:rw -v ",analysis_dir,":/data:rw bblum/omics_notebook Rscript '/home/src/Pipeline.R' '/home' '/data'"])
+elif str(sys.argv[1])=="Docker" :
+  print("Running with Docker.")
+  subprocess.Popen(args=["docker run -it --rm -u docker -v ", notebook_path,":/home:rw -v ",analysis_dir,":/data:rw bblum/omics_notebook Rscript '/home/src/Pipeline.R' '/home' '/data'"])
 
 # 3. Run with Singularity
-# singularity_image = "/project/cnsbomic/Omics_Notebook/ON2.simg" # PATH/TO/SINGULARITY IMAGE
-# subprocess.Popen(args=["singularity run --bind ",notebook_path,":'/home':rw --bind ",analysis_dir,":'/data':rw  ",singularity_image," Rscript '/home/src/Pipeline.R' '/home' '/data'"])
+elif str(sys.argv[1])=="Singularity" and len(sys.argv)==3 :
+  print("Running with Singularity.")
+  singularity_image = sys.argv[2] # PATH/TO/SINGULARITY IMAGE
+  subprocess.Popen(args=["singularity run --bind ",notebook_path,":'/home':rw --bind ",analysis_dir,":'/data':rw  ",singularity_image," Rscript '/home/src/Pipeline.R' '/home' '/data'"])
