51f9e0e0a6bdd597b42d7b36087c60c912036294
Christian Heckendorf <heckendorfc@gmail.com>
2022-02-18 16:22:57 -0500

    hide venn diagram spam

--- R/drawVenn.R
+++ R/drawVenn.R
@@ -15,10 +15,10 @@ drawVenn <- function(item_list, item_name, outputpath=output_plots_path){
     item_list <- item_list[1:5]
   }
   fill_col <- grDevices::rainbow(length(item_list))
-  #futile.logger::flog.threshold(futile.logger::ERROR);
+  futile.logger::flog.threshold(futile.logger::ERROR, name="VennDiagramLogger");
   venn <- VennDiagram::venn.diagram( x=item_list, filename=NULL,lty="blank",# height=2000, width=2000, 
                         cat.default.pos="outer", fill=fill_col, main=paste("Overlap: ", item_name, sep=""), 
-                        fontfamily="sans", ext.text=FALSE);
+                        fontfamily="sans", ext.text=FALSE, disable.logging=T);
   
   output_filename <- file.path(outputpath, paste("VennDiagram_", item_name,".pdf", sep="") );
   pdf(output_filename, width=4, height=3);
