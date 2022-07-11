be8c51dba4ddd45e65aadd8c307d9468592e86fe
Christian Heckendorf <heckendorfc@gmail.com>
2022-07-11 19:09:26 -0400

    write MOMENTA GMT files to output dir

--- R/g_enrichment.R
+++ R/g_enrichment.R
@@ -297,7 +297,9 @@ g.gsea.momenta = function(g, working_dir="3_GSEA_MOMENTA", deps=T){
   g$gmt_files = lapply(file.path("matched",g$gmt_files),FUN=get.data.fileconn)
 
   for(i in 1:length(g$gmt_files)){
-    g = g.gsea(g, gmt=g$gmt_files[[i]], gmt_name=g$gmt_names[i], working_dir=working_dir)
+    outfile <- file.path(g$gmt_path, paste0(g$gmt_names[i],".gmt"))
+    cat(readLines(g$gmt_files[[i]]), file=outfile)
+    g = g.gsea(g, gmt=outfile, gmt_name=g$gmt_names[i], working_dir=working_dir)
   }
 
   g$calls = c(g$calls, "g.gsea.momenta")
